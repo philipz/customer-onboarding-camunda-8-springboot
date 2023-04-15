ea4fdfd7d2e7ed8fa31582b74b47bf59c4852830
philipz <philipzheng@gmail.com>
2023-04-15 21:00:47 +0800

    Upgrade to 3.0.5

--- src/main/java/io/berndruecker/onboarding/customer/CustomerOnboardingSpringbootApplication.java
+++ src/main/java/io/berndruecker/onboarding/customer/CustomerOnboardingSpringbootApplication.java
@@ -1,12 +1,10 @@
 package io.berndruecker.onboarding.customer;
 
-import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableZeebeClient
 @Deployment(resources = "classpath:customer-onboarding.bpmn")
 public class CustomerOnboardingSpringbootApplication {
 
--- src/main/java/io/berndruecker/onboarding/customer/process/ProcessConstants.java
+++ src/main/java/io/berndruecker/onboarding/customer/process/ProcessConstants.java
@@ -5,5 +5,4 @@ public class ProcessConstants {
   public static final String VAR_SCORING_REQUEST_ID = "scoringRequestId";
   public static final String VAR_SCORING_RETRY_COUNT = "scoringRetryCount";
   public static final String VAR_SCORING_RESULT = "scoringResult";
-
 }
